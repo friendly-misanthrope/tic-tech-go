dfb56627c11f51f8d599953876b6cd92c9f92109
friendly-misanthrope <nickthedevguy@gmail.com>
2024-11-14 02:58:38 -0800

    Added link and NavButton for navigating home; added NavButtons for tickets & customers pages

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,25 @@
+import { HomeIcon, File, UsersRound } from "lucide-react"
+import Link from 'next/link'
+import { NavButton } from "@/components/NavButton"
+
 export default function Header() {
   return (
-    <p>Header</p>
+    <header>
+      <div>
+        
+        <div>
+          <NavButton icon={HomeIcon} label="Home" href="/home" />
+          <Link href="/home" title="Home">
+            <h1>Leo&apos;s Device Repair</h1>
+          </Link>
+        </div>
+
+        <div>
+          <NavButton icon={File} label="Tickets" href="/tickets" />
+          <NavButton icon={UsersRound} label="Customers" href="/tickets" />
+        </div>
+
+      </div>
+    </header>
   )
 }
