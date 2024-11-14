2fcc34c1ea9f924ed4e5201717238aa5afb72783
friendly-misanthrope <nickthedevguy@gmail.com>
2024-11-14 01:03:48 -0800

    Added TW classes on homepage parent div and main element

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,9 +2,9 @@ import Link from "next/link"
 
 export default function Home() {
   return (
-    <div>
+    <div className="bg-black bg-home-img bg-cover bg-center">
 
-      <main>
+      <main className="flex flex-col justify-center text-center max-w-5xl mx-auto h-dvh">
         <div>
           <h1>Leo&apos;s Computer <br/>Repair</h1>
           <address>
