e2b45e2d7c83f9bfa58a8801ff7bd8cb56f61a4b
friendly-misanthrope <nickthedevguy@gmail.com>
2024-11-14 01:19:31 -0800

    changed gap from 6 to 4, add additional container for 'hours' section, boldened header font

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,15 +5,20 @@ export default function Home() {
     <div className="bg-black bg-home-img bg-cover bg-center">
 
       <main className="flex flex-col justify-center text-center max-w-5xl mx-auto h-dvh">
-        <div>
-          <h1>Leo&apos;s Computer <br/>Repair</h1>
+        <div className="flex flex-col gap-4 p-12 rounded-xl bg-black/90 w-4/5 sm:max-w-96 mx-auto text-white sm:text-2xl">
+          <h1 className="font-extrabold">Leo&apos;s Device Repair</h1>
           <address>
             400 Broad Street<br />
             Seattle, WA 98109
           </address>
-          <p>
-            Open Daily 9am -5pm
-          </p>
+          <div>
+            <h2 className="font-bold">Hours: </h2>
+            <p>
+              Mon-Fri: 9am to 5pm<br />
+              Sat: 11am to 4pm<br />
+            </p>
+          </div>
+          
           <Link href="tel:5559052100" className="hover:underline">
             (206)555-2100
           </Link>
