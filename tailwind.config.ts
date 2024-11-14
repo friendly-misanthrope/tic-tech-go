c3f870cf14b96dffdf67332a5cbd8cedaa7b99c0
friendly-misanthrope <nickthedevguy@gmail.com>
2024-11-14 00:51:02 -0800

    configured backgroundImage

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -9,6 +9,9 @@ export default {
   ],
   theme: {
   	extend: {
+			backgroundImage: {
+				"home-img": "url('/img/home-img.jpg')"
+			},
   		colors: {
   			background: 'hsl(var(--background))',
   			foreground: 'hsl(var(--foreground))',
