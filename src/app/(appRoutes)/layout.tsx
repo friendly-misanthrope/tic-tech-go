fb0a0dda2afd7e6123c18f2f6718faa1efa97459
friendly-misanthrope <nickthedevguy@gmail.com>
2024-11-14 22:42:58 -0800

    changed erroneous className in app layout

--- src/app/(appRoutes)/layout.tsx
+++ src/app/(appRoutes)/layout.tsx
@@ -6,7 +6,7 @@ export default async function AppLayout({
   children: React.ReactNode
 }) {
   return (
-    <div className="mx-auto w-full max-width-8xl">
+    <div className="mx-auto w-full max-w-7xl">
       <Header />
       <div className="px-4 py-2">
         { children }
