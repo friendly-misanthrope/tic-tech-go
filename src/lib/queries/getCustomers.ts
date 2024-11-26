bff1fed602ec945bfc35eed677ca79e495f717df
friendly-misanthrope <nickthedevguy@gmail.com>
2024-11-26 09:01:56 -0800

    created file for ticket select queries, implemented selectById

--- src/lib/queries/getCustomers.ts
+++ src/lib/queries/getCustomers.ts
@@ -2,7 +2,7 @@ import { db } from "@/db"
 import { customers } from "@/db/schema"
 import { eq } from "drizzle-orm"
 
-export async function getCustomer(id: number) {
+export async function getCustomerById(id: number) {
   const customer = await db.select()
     .from(customers)
     .where(eq(customers.id, id))
